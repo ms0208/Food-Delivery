ea3920ca463eb0a7446e3f51e1aff726ab8501d1
ms0208 <manav.8230680@gmail.com>
2025-02-01 10:11:00 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import foodRouter from './routers/foodrouter.js';
 import userRouter from './routers/userrouter.js';
 import 'dotenv/config'
 import cartRouter from './routers/cartrouter.js';
+import orderRouter from './routers/orderrouter.js';
 
 // app config
 const app = express();
@@ -27,6 +28,7 @@ app.use('/api/food',foodRouter);
 app.use('/images',express.static('uploads'))
 app.use('/api/user',userRouter);
 app.use('/api/cart',cartRouter);
+app.use('/api/order',orderRouter);
 
 app.get("/",(req,res)=>{
     res.send("API WORKING");
